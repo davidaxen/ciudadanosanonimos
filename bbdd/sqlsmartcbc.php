933a500028fc2b9c5caed581ba6d880205f04cbb
david <davidbra2000@gmail.com>
2020-07-30 13:04:02 +0200

    cambios bbdd

--- bbdd/sqlsmartcbc.php
+++ bbdd/sqlsmartcbc.php
@@ -1,6 +1,8 @@
 <?php
 date_default_timezone_set('Europe/Madrid');
-$dbh=mysql_connect ("localhost", "bbddsmartcbc", "Jas170174#") or die ('I cannot connect to the database because: ' . mysql_error());
+//$dbh=mysql_connect ("localhost", "bbddsmartcbc", "Jas170174#") or die ('I cannot connect to the database because: ' . mysql_error());
+$dbh=mysql_connect ("localhost", "root", "") or die ('I cannot connect to the database because: ' . mysql_error());
+
 mysql_select_db ("bbddciudadanos");
 mysql_query("SET CHARACTER SET latin1");
 mysql_query("SET NAMES latin1");
