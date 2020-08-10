28c30f008ca6a63403b95c62197c42a0d0089ad4
david <davidbra2000@gmail.com>
2020-08-10 11:33:23 +0200

    cambios 10 08

--- servicios_n/mensaje/introrespuesta.php
+++ servicios_n/mensaje/introrespuesta.php
@@ -17,7 +17,8 @@ if ($ide!=null){;
 $sql1 = "INSERT INTO respuestamensajes (idempresa,idempleado,idmensaje,respuesta,textorespuesta) VALUES 
 ('$ide','$idtrab','$id','$respuesta','$textotro')";
 //echo $sql1;
-$result1=mysqli_query ($conn,$sql1) or die ("Invalid result ipuntcont1");
+$result1=$conn->exec($sql1);
+//$result1=mysqli_query ($conn,$sql1) or die ("Invalid result ipuntcont1");
 
 
 
