ad5727c09b8c3488aae36c09ebfdde88feb4c4a2
brayan camilo <brayan199928@hotmail.com>
2020-12-16 20:01:14 +0100

    chaat

--- chat/index.php
+++ chat/index.php
@@ -59,17 +59,18 @@
     
     <div class="panel-body">
 
-       <?php
-              if(file_exists("log.html") && filesize("log.html") > 0){
-                $handle = fopen("log.html", "r");
-                 $contents = fread($handle, filesize("log.html"));
-                fclose($handle);
-          ?>
+
 
           <div class="message">
             <div class="message-text-wrapper">
               <div class="message-text" id="chatbox" >
-                <?php        
+
+          <?php
+              if(file_exists("log.html") && filesize("log.html") > 0){
+                $handle = fopen("log.html", "r");
+                 $contents = fread($handle, filesize("log.html"));
+                fclose($handle);
+       
                   echo $contents;
                   }
                 ?>
