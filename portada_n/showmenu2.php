088d60aaf31f2faaf8ff7790ea6ee4038ddff853
Nicolás N.A <>
2021-02-01 16:04:22 +0100

    path arreglado

--- portada_n/showmenu2.php
+++ portada_n/showmenu2.php
@@ -8,7 +8,7 @@
    <ul class="[ nav navbar-nav navbar-right ]">
     <li><a href="ultimasentradas.php" class="[ animate ]" onclick="openCity(event, 'd0')" >ULTIMAS ENTRADAS</a></li>
     <li><a href="ultimasincidencias.php" class="[ animate ]" onclick="openCity(event, 'd0')" >ULTIMAS INCIDENCIAS</a></li>
-    <li><a href="dpuntcont.php" class="[ animate ]" onclick="openCity(event, 'd0')" >CREAR PREGUNTA</a></li>
+    <li><a href="../servicios_n/mensaje/dpuntcont.php" class="[ animate ]" onclick="openCity(event, 'd0')" >CREAR PREGUNTA</a></li>
     <li><a href="salir.php" class="[ animate ]" onclick="openCity(event, 'd0')" >LOG OUT</a></li>
  </ul>
 </div>
--- servicios_n/mensaje/bbdd.php
+++ servicios_n/mensaje/bbdd.php
@@ -1,5 +1,5 @@
-<?php 
-include('../bbdd/sqlsmartcbc-3.php');
+<?php
+include('../../bbdd/sqlsmartcbc-3.php');
 extract($_COOKIE);
 extract($_POST);
 extract($_GET);
