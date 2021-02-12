9af572ca974b0a833543f1ad17645f40da83e7ee
brayan camilo <brayan199928@hotmail.com>
2021-02-13 00:24:38 +0100

    13 02

--- incidencias_t.php
+++ incidencias_t.php
@@ -5,15 +5,10 @@ include('bbdd.php');
 <html>
 
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Convergence" />
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="incidencias_t.css">
 <link rel="stylesheet" type="text/css" href="portada_n/cabecera.css">
 <link rel="stylesheet" type="text/css" href="nav.js">
@@ -32,7 +27,6 @@ include('bbdd.php');
 </script>
 
 <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 
 <!--<strong><p style="text-align: center; font-family: Palatino, 'Palatino Linotype', serif; font-size: 18px;">Estamos trabajando en ello...</p></strong>-->
 
@@ -48,18 +42,6 @@ include('bbdd.php');
 	    margin-left: auto;
 	    margin-right: auto;
 	}
-
- @media (min-width: 1192px){
-      .external-collapse.navbar-collapse {
-          display: -webkit-box!important;
-          display: -ms-flexbox!important;
-          display: flex!important;
-      }
-  }
-
-
-
-
 </style>
 
 		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
@@ -72,7 +54,7 @@ include('bbdd.php');
 
 
 		<nav class="[ navbar navbar-fixed-top ][ navbar-bootsnipp animate ]" role="navigation">
-  			<table >
+  					<table align="center">
 		<tr>
 			<td>
 	    		<div class="[ navbar-header ]">
@@ -83,36 +65,17 @@ include('bbdd.php');
 	    		</div>
 	    	</td>
 			<td>
-
-
-
-	<nav class="navbar navbar-expand-lg navbar-light px-0 py-0 mb-5">
-        <div class="container-fluid">
-            <div class="navbar-header" style="width: 100%">
-                <button type="button" class="navbar-toggler pull-right my-2" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="pull-left">
-                </div>
-            </div>
-            <div class="navbar-collapse collapse">
-            	<div>
+				<div align="center" >
 				<?php
 					include_once("portada_n/showmenu.php");
 
 				?>	
 				<td>
-			      	<div>
-						<?php include ('donaciones/index.php')?>
+			      	<div style="float: right;">
+								<?php include ('donaciones/index.php')?>
 					</div>
 				</td>
 			</div>
-            </div>
-        </div>
-    </nav>
-
-
-
 			</td>
 		</tr>
 	</table>
