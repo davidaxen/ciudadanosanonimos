5d756cd48b853eb00e8b83a6aab7912c68569715
Nicolás N.A <niko.niebles7@outlook.com>
2021-02-17 12:14:08 +0100

    responsive navbar sin terminar

--- incidencias_t.php
+++ incidencias_t.php
@@ -1,14 +1,17 @@
-<?php   
+<?php
 include('bbdd.php');
 
 ?>
 <html>
 
 <head>
+
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Convergence" />
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="incidencias_t.css">
 <link rel="stylesheet" type="text/css" href="portada_n/cabecera.css">
 <link rel="stylesheet" type="text/css" href="nav.js">
@@ -27,7 +30,7 @@ include('bbdd.php');
 </script>
 
 <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 <!--<strong><p style="text-align: center; font-family: Palatino, 'Palatino Linotype', serif; font-size: 18px;">Estamos trabajando en ello...</p></strong>-->
 
 <style type="text/css" media="print">
@@ -42,6 +45,14 @@ include('bbdd.php');
 	    margin-left: auto;
 	    margin-right: auto;
 	}
+	@media (min-width: 1192px){
+			 .external-collapse.navbar-collapse {
+					 display: -webkit-box!important;
+					 display: -ms-flexbox!important;
+					 display: flex!important;
+			 }
+	 }
+
 </style>
 
 		<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
@@ -66,24 +77,38 @@ include('bbdd.php');
 	    		</div>
 	    	</td>
 			<td style="width: 65%; ">
-				<div>
-					<?php
-						include_once("portada_n/showmenu.php");
+				<nav class="navbar">
+									<div class="navbar-header">
+											<button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+											<span class="navbar-toggler-icon"></span>
+											</button>
+											<div class="pull-left">
+											</div>
+									</div>
+									<div class="navbar-collapse collapse">
+										<div>
+							<?php
+								include_once("portada_n/showmenu.php");
 
-					?>	
-					
-				</div>
-			</td>
-			<td>
-		      	<div>
-							<?php include ('donaciones/index.php')?>
-				</div>
-			</td>
-		</tr>
-	</table>
- 	</nav>
+							?>
+							<td>
+										<div>
+									<?php include ('donaciones/index.php')?>
+								</div>
+							</td>
+						</div>
+									</div>
+
+					</nav>
+
+
+
+						</td>
+					</tr>
+				</table>
+				</nav>
 <div class='wrapper fadeInDown' >
-<div id='formContent' >	
+<div id='formContent' >
 	<i class="fas fa-exclamation-triangle  fa-lg fa-fw" style="font-size: 30px"></i>
 <table style=" display: flex; justify-content: center; margin-top: 15%;" >
 	<tr>
@@ -96,7 +121,7 @@ include('bbdd.php');
 	<tr><td>&nbsp;</td></tr>
 	<tr><td>&nbsp;</td></tr>
 	<tr>
-		<td>	
+		<td>
 			<form method="POST" action="introincidencias.php" onsubmit="return validateForm()">
 				<div class="input-group">
 					<div class="input-group-prepend">
