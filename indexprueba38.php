309eb270033d5842310ddc08d4f69f5414ed961d
Nicolás N.A <niko.niebles7@outlook.com>
2021-02-18 16:55:24 +0100

    usr preguntas nav

--- indexprueba38.php
+++ indexprueba38.php
@@ -6,34 +6,86 @@ if ($com=='comprobacion'){;
 include('portada_n/cabecera.php');
 ?>
 
+<meta charset="utf-8">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Convergence" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="portada_n/boostrapUlt.css">
 <link rel="stylesheet" type="text/css" href="nav.js">
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-<body style="background-image:url(../img/iconos/portada_ca.jpg)";>
-	<div id="main">
-	<div class="titulo">
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<style>
+@media (min-width: 1192px){
+		 .external-collapse.navbar-collapse {
+				 display: -webkit-box!important;
+				 display: -ms-flexbox!important;
+				 display: flex!important;
+		 }
 
-	</div>
-	</div>
-<nav class="[ navbar navbar-fixed-top ][ navbar-bootsnipp animate ]" role="navigation">
-		<div class="[ navbar-header ]">
-				<div class="[ animbrand ]">
-						<a class="[ navbar-brand ][ animate ]" href="../inicio1.php"><img src="../img/ciudadanoslogo.png"></a>
-				</div>
-		</div>
-	<div class="[ container ]">
-	<?php
-		include_once("portada_n/showmenu3.php");
-	?>
-	</div>
-</nav>
+ }
+
+ @media (min-width: 768px){
+		 .hid {
+					visibility: hidden;
+		 }
+}
+</style>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=ISO-8859-1">
+<body   style="background-image:url(../img/iconos/portada_ca.jpg)"; >
+
+
+		<nav class="[ navbar navbar-fixed-top ][ navbar-bootsnipp animate ]" role="navigation">
+
+  		<table style="margin-left: 20px; width: 100%">
+		<tr>
+			<td style="width: 20%; ">
+	    		<div class="[ navbar-header ]">
+	        		<div class="[ animbrand ]">
+	            		<a style="float: none;" class="[ navbar-brand ][ animate ]" href="../inicio1.php"><img src="../img/ciudadanoslogo.png"></a>
+
+	        		</div>
+	    		</div>
+	    	</td>
+			<td style="width: 65%; ">
+				<nav class="navbar navbar-expand-lg navbar-light">
+									<div class="navbar-header">
+											<button type="button" class="navbar-toggler hid" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+											Menu
+											</button>
+											<div class="pull-left">
+											</div>
+									</div>
+									<div class="navbar-collapse collapse">
+										<div>
+							<?php
+								include_once("portada_n/showmenu3.php");
+
+							?>
+							<td>
+										<div>
+									<?php include ('donaciones/index.php')?>
+								</div>
+							</td>
+						</div>
+									</div>
+
+					</nav>
+
+
+
+						</td>
+					</tr>
+				</table>
+				</nav>
 
 <?php
 $sql="select * from portadapag,paginapor where paginapor.idpag=portadapag.idpag and idempresa='".$ide."' order by idportada asc";
@@ -70,7 +122,7 @@ $j=$j+1;
 </body>
 
 </html>
-<?php 
+<?php
 header("Location: portada_n/ultimasincidencias.php");
 }else{;
 include ('cierre.php');
--- portada_n/ultimasincidencias.php
+++ portada_n/ultimasincidencias.php
@@ -31,31 +31,90 @@ function refrescar()
 <style type="text/css" media="print">
 .nover {display:none}
 </style>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Convergence" />
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="boostrapUlt.css">
 <link rel="stylesheet" type="text/css" href="nav.js">
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+<script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+<style>
+@media (min-width: 1192px){
+		 .external-collapse.navbar-collapse {
+				 display: -webkit-box!important;
+				 display: -ms-flexbox!important;
+				 display: flex!important;
+		 }
+
+ }
+
+ @media (min-width: 768px){
+		 .hid {
+					visibility: hidden;
+		 }
+}
+</style>
+
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta http-equiv="content-type" content="application/xhtml+xml; charset=ISO-8859-1">
 <!--<body onload="setTimeout('refrescar()', 5000);">-->
-<body style="background-image:url(../img/iconos/portada_ca.jpg)";>
-<nav class="[ navbar navbar-fixed-top ][ navbar-bootsnipp animate ]" role="navigation">
-		<div class="[ navbar-header ]">
-				<div class="[ animbrand ]">
-						<a class="[ navbar-brand ][ animate ]" href="../inicio1.php"><img src="../img/ciudadanoslogo.png"></a>
-				</div>
-		</div>
-	<div class="[ container ]">
-	<?php
-		include_once("showmenu2.php");
-	?>
-	</div>
-</nav>
+<body   style="background-image:url(../img/iconos/portada_ca.jpg)"; >
+
+
+		<nav class="[ navbar navbar-fixed-top ][ navbar-bootsnipp animate ]" role="navigation">
+
+  		<table style="margin-left: 20px; width: 100%">
+		<tr>
+			<td style="width: 20%; ">
+	    		<div class="[ navbar-header ]">
+	        		<div class="[ animbrand ]">
+	            		<a style="float: none;" class="[ navbar-brand ][ animate ]" href="../inicio1.php"><img src="../img/ciudadanoslogo.png"></a>
+
+	        		</div>
+	    		</div>
+	    	</td>
+			<td style="width: 65%; ">
+				<nav class="navbar navbar-expand-lg navbar-light">
+									<div class="navbar-header">
+											<button type="button" class="navbar-toggler hid" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+											Menu
+											</button>
+											<div class="pull-left">
+											</div>
+									</div>
+									<div class="navbar-collapse collapse">
+										<div>
+							<?php
+								include_once("showmenu3.php");
+
+							?>
+							<td>
+										<div>
+									<?php include ('../donaciones/index.php')?>
+								</div>
+							</td>
+						</div>
+									</div>
+
+					</nav>
+
+
+
+						</td>
+					</tr>
+				</table>
+				</nav>
 <br> <br> <br> <br> <br> <br> <br>
 <div class="container fadeInDown" style="background-color: white; border-radius: 10px;">
 <table style="width:100%;">
